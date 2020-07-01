cf73bcc0aa40c742f8a92159acb25b44c1400f76
root <tilman.jaeger@web.de>
2020-07-01 10:31:37 +0000

    Task 4

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -18,6 +18,9 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> uLock(_mutex);
+    _queue.push_back(std::move(msg));
+    _condition.notify_one();
 }
 
 
@@ -83,7 +86,7 @@ void TrafficLight::cycleThroughPhases()
 
             }
 
-            //_messageQueue.send(std::move(_currentPhase));
+            _messageQueue.send(std::move(_currentPhase));
 
             currentPointIntime = std::chrono::system_clock::now();
         }     
